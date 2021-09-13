684cff87654882d67699964f932ed0d5760421e7
Brooks McKinley <brooksmckinley@yahoo.com>
2021-09-13 13:44:44 -0400

    Initial commit

---
+++ src/test/java/com/brooksmckinley/AppTest.java
@@ -0,0 +1,12 @@
+package com.brooksmckinley;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
