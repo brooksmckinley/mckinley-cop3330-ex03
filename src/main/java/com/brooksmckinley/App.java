4af61e08c87ab37d9d865a1e0057bc9278a48aa3
Brooks McKinley <brooksmckinley@yahoo.com>
2021-09-13 13:56:34 -0400

    Implemented input/output

--- src/main/java/com/brooksmckinley/App.java
+++ src/main/java/com/brooksmckinley/App.java
@@ -1,7 +1,17 @@
 package com.brooksmckinley;
 
+import java.util.Scanner;
+
 public class App {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = scan.nextLine();
+        System.out.print("Who said it? ");
+        String author = scan.nextLine();
+        System.out.println(generateOutput(author, quote));
+    }
     public static String generateOutput(String author, String quote) {
         return author + " says, \"" + quote + "\"";
     }
